994f7a1a66424af485ee46f9dc35e37ef131047c
yhy200103 <55180890+yhy200103@users.noreply.github.com>
2019-11-25 19:01:31 +0800

    Delete sasa.js

--- sasa.js
+++
@@ -1,9 +0,0 @@
-function stat() {
-    let str = document.getElementById("str").value
-    let arr = str.split('').sort()
-    obj = arr.reduce((pre, item) => {
-        pre[item] ? pre[item]++ : pre[item] = 1
-        return pre
-    }, {})
-    document.getElementById('result').innerText = JSON.stringify(obj)
-}
